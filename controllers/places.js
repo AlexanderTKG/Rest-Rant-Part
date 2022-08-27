09fcee59c9ffb0fc8abff6476fa200688c9bf6d9
AlexanderTKG <Alexander-MD@outlook.com>
2022-08-27 12:44:44 -0400

    Update

--- controllers/places.js
+++ controllers/places.js
@@ -1,23 +1,31 @@
 const router = require('express').Router()
 
-// GET/places/new route
-router.get('/new', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/coffee-1711012_640.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/brick-wall-1834784_640.jpg'
-      }]
-    res.render('places/new', {places})
-
+// GET/places
+router.get('/', (req, res) => {
+  let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/coffee-1711012_640.jpg'
+},
+{
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/brick-wall-1834784_640.jpg'   
+}
+]
+  res.render('places/index', {places})
   })
+
+  router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+
+
+
 
 
 
