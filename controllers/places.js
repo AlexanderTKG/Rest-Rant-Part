74319b342993847624267b7097ef9941309936dd
AlexanderTKG <Alexander-MD@outlook.com>
2022-08-29 18:46:59 -0400

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -31,5 +31,20 @@ router.post('/', (req, res) => {
   res.send('POST /places')
 })
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id]})
+  }
+})
+
+
+
 
 module.exports = router
