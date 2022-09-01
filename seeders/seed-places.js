e40fdd72dfb3d4e1bdd5d716d6a291e3a0d8c868
AlexanderTKG <Alexander-MD@outlook.com>
2022-09-01 13:08:41 -0400

    mongodb

---
+++ seeders/seed-places.js
@@ -0,0 +1,25 @@
+const db = require('../models')
+
+db.Place.create([{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: 'public/images/brick-wall-1834784_640.jpg',
+    founded: 1989
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: 'public/images/coffee-1711012_640.jpg',
+    founded: 2020
+}])
+.then(() => {
+    console.log('Success!')
+    process.exit()
+})
+.catch(err => {
+    console.log('Failure!', err)
+    process.exit()
+})
